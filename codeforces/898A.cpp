fbc65b5cb857114dbdbae9824a8e6d856b6583bb
LordRonz <aarontanhar2000@gmail.com>
2021-04-30 12:33:10 +0700

    contest 1519

---
+++ codeforces/898A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+#define MAX(a,b,c) max(a,max(b,c))
+#define MIN(a,b,c) min(a,min(b,c))
+#define MP make_pair
+#define FOR(i, a, b) for(int i = a; i < b; ++i)
+#define gcd __gcd
+typedef pair<int, int> pii;
+typedef long long ll;
+const int MAX_N = 1e5 + 1;
+//0xACCE97ED;
+
+int main() {
+	long n;
+	scanf("%ld", &n);
+	double a = n / 10.0f;
+	printf("%ld\n", (long)round(a) * 10);
+	return 0;
+}
